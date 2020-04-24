22983c97ca2b71bfab80517201fb437fec2afc74
potato <stoneskc@163.com>
2020-04-24 17:45:01 +0800

    文章列表分页

--- src/pages/Article/List/index.js
+++ src/pages/Article/List/index.js
@@ -3,8 +3,11 @@ import { withRouter, } from 'react-router-dom';
 
 import './index.css';
 import { get } from '../../../utils/request';
+import { List, Avatar } from 'antd';
+import { MessageOutlined, LikeOutlined, StarOutlined } from "@ant-design/icons";
 
-function List(props) {
+
+function ArticleList(props) {
     const [data, setData] = useState([]);
     useEffect(() => {
         get('/article').then(res => {
@@ -12,10 +15,55 @@ function List(props) {
         })
     }, []);
 
+    const IconText = ({ icon, text }) => (
+        <span>
+            {React.createElement(icon, { style: { marginRight: 8 } })}
+            {text}
+        </span>
+    );
+
     return (
         <div>
             {console.log(data, 'data')}
-            {
+            <List
+                itemLayout="vertical"
+                size="large"
+                pagination={{
+                    onChange: page => {
+                        console.log(page);
+                    },
+                    pageSize: 3,
+                }}
+                dataSource={data}
+                // header={`${data.length} ${data.length > 1 ? 'replies' : 'reply'}`}
+                itemLayout="horizontal"
+                // renderItem={(topComments) => articleList([topComments], comments)}
+                renderItem={item => (
+                    <List.Item
+                        key={item.title}
+                        actions={[
+                            <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+                            <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+                            <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+                        ]}
+                        extra={
+                            <img
+                                width={272}
+                                alt="logo"
+                                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                            />
+                        }
+                    >
+                        <List.Item.Meta
+                            avatar={<Avatar src={item.avatar} />}
+                            title={<a href={item.href}>{item.title}</a>}
+                            description={item.description}
+                        />
+                        {item.content}
+                    </List.Item>
+                )}
+            />
+            {/* {
                 data.map(item => {
                     return <div className="list_box" onClick={() => props.history.push(`/home/article/${item._id}`)}>
                         <div className="list_title">
@@ -34,9 +82,9 @@ function List(props) {
                         </div>
                     </div>
                 })
-            }
+            } */}
         </div>
     )
 }
 
-export default withRouter(List);
+export default withRouter(ArticleList);
